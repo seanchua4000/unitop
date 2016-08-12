9ad591d238b259e7f63ddc8eaf98a73eb9abf6f7
Sean <dulcis.fate@gmail.com>
2016-08-12 10:25:49 +0800

    Facebook Login

--- fb2/index.php
+++ fb2/index.php
@@ -62,7 +62,7 @@ if (isset($accessToken)) {
     // Now you can redirect to another page and use the access token from $_SESSION['facebook_access_token']
 } else {
   // replace your website URL same as added in the developers.facebook.com/apps e.g. if you used http instead of https and you used non-www version or www version of your website then you must add the same here
-  $loginUrl = $helper->getLoginUrl('http://localhost/unitop2/fb2/index.php', $permissions);
+  $loginUrl = $helper->getLoginUrl('http://www.wagsc.com/fb2/index.php', $permissions);
 
   echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 }
