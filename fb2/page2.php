ddde77f50a4a5ac1043d911c0a7c669339d06f6d
Sean <dulcis.fate@gmail.com>
2016-08-13 11:02:51 +0800

    Facebook Share

--- fb2/page2.php
+++ fb2/page2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<title>Share!</title>
-	<meta property="og:url"			content="http://www.wagsc.com/fb2/page2.php" />
+	<meta property="og:url"			content="http://www.wagsc.com/" />
 	<meta property="og:type"    	content="Shopping!" />
 	<meta property="og:title"		content="Unitop Online Shopping" />
 	<meta property="og:description"	content="Unitop Online Shopping the most exciting way to do your shopping" />
@@ -36,7 +36,7 @@
   data-show-faces="false">
 </div>
 
-	<p>Where does it come from?1w
+	<p>qweqweWhere does it come from?1wsada
 Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
 
 The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
