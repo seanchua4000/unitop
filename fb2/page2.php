d1e6c737d60bccc90de9056df98dc87ea9d7c226
Sean <dulcis.fate@gmail.com>
2016-08-30 14:55:31 +0800

    login panel

--- fb2/page2.php
+++ fb2/page2.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Share!</title>
-	<meta property="og:url"			content="http://www.wagsc.com/" />
-	<meta property="og:type"    	content="Shopping!" />
-	<meta property="og:title"		content="Unitop Online Shopping" />
-	<meta property="og:description"	content="Unitop Online Shopping the most exciting way to do your shopping" />
-	<meta property="og:site_name"	content="Unitop" />
-	<meta property="og:image"		content="hello.jpg" />
+	<title>Testing Site</title>
+	<meta property="og:url"			content="http://testersean.comli.com/unitop2/fb2/page2.php" />
+	<meta property="og:type"    	content="website" />
+	<meta property="og:title"		content="UNITOP Online Shopping" />
+	<meta property="og:image"		content="http://testersean.comli.com/unitop2/fb2/image.jpg" />
+	<meta property="og:description"	content="UNITOP YOUR BUDGET-FRIENDLY STORE" />
 	<meta property="fb:app_id"		content="1761464340768101" />
 </head>
 <body>
-<script>
-  window.fbAsyncInit = function() {
+	<div id="fb-root"></div>
+	<script>
+	window.fbAsyncInit = function() {
     FB.init({
       appId      : '1761464340768101',
       xfbml      : true,
@@ -20,25 +20,31 @@
     });
   };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-</script>
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=1761464340768101";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+	</script>
 
-<div
-  class="fb-like"
-  data-share="true"
-  data-width="450"
-  data-show-faces="false">
-</div>
 
-	<p>qweqweWhere does it come from?1wsada
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+	<div class="fb-like"
+		data-href="http://testersean.comli.com/unitop2/fb2/page2.php"
+		data-layout="button_count"
+		data-action="recommend"
+		data-size="small" 
+		data-show-faces="false"
+		data-share="false">
+	</div>
 
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+	<div class="fb-share-button"
+		data-href="http://testersean.comli.com/unitop2/fb2/page2.php"
+		data-layout="button_count"
+		data-size="small"
+		data-mobile-iframe="true">
+		<a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Share</a>
+	</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Testing Website</title>
-</head>
+	<meta property="og:title"		content="Unitop Online Shopping" />
+	<meta property="og:url"			content="http://testersean.comli.com/unitop2/fb2/page2.php" />
+	<meta property="og:type"    	content="Shopping!" />
+	
+	<meta property="og:image"		content="hello.jpg" />
+	<meta property="og:description"	content="Unitop Online Shopping the most exciting way to do your shopping" />
+	<meta property="og:site_name"	content="Unitop" />
+	
+	<meta property="fb:app_id"		content="1761464340768101" /></head>
 <body>
 <h1>Hello World!</h1>
 </body>
